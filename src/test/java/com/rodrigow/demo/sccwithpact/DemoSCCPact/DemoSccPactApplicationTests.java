d5a11ce85fed7023c865dfe0a22ff8f92143376b
Rodrigo Wolschick <rodrigowolschick@gmail.com>
2022-05-03 19:57:12 -0300

    spring initial code

---
+++ src/test/java/com/rodrigow/demo/sccwithpact/DemoSCCPact/DemoSccPactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigow.demo.sccwithpact.DemoSCCPact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSccPactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
